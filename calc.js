72881052a550465c07843edaa2cd857260ab9f7b
Sam Shock <sam.shock924@gmail.com>
2022-01-06 15:00:46 -0500

    first operand stored

--- calc.js
+++ calc.js
@@ -1,15 +1,34 @@
+let firstOperand = null;
+let secondOperand = null;
+let operation = null;
+let firstOperandChosen = false;
+let secondOperandChosen = false;
+
+
 const calcDisplay = document.querySelector('#calculatorDisplay');
 const operatorButtons = document.querySelectorAll('.operatorButton');
 const numberButtons = document.querySelectorAll('.numButton');
+// let numberValue = calcDisplay.value;
 
 operatorButtons.forEach(operatorButtons => {
     operatorButtons.addEventListener('click', () => {
-        calcDisplay.value = operatorButtons.innerText;
+        console.log(operatorButtons.innerText);
+        firstOperandChosen = true;
 })
 });
 
 numberButtons.forEach(numberButtons => {
     numberButtons.addEventListener('click', () => {
-        calcDisplay.value = numberButtons.innerText;
+        if (firstOperandChosen == true) {
+            firstOperand = calcDisplay.value;
+            calcDisplay.value = '';
+            calcDisplay.value += numberButtons.textContent;
+            firstOperandChosen = false;
+            console.log(firstOperand);
+        }
+        else {
+            calcDisplay.value += numberButtons.textContent;
+        }
 })
 });
+
